3480c9f239ecd32f0286a0f2eb17d1bf8948a8c9
sonsolutions <117684726+sonsolutions@users.noreply.github.com>
2025-03-04 17:53:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Matriz de pokemons e suas informações
+
 let pokemons = [
   [1,"poochyena", 2 , "M", 13, 13],
   [2,"zigzagoon", 2 , "F", 13, 13],
@@ -7,6 +9,8 @@ let pokemons = [
   [6,"poochyena", 3 , "F", 24, 15],
   [7, "wurmple" , 2 , "M", 7, 15]
 ]
+
+//Resposta do console log de cada uma das linhas das matrizes
 console.l0g(" O pokemon da posição " + pokemons[0][0] + " nomeado como " + pokemons[0][1] + " no level "+ pokemons[0][2] + " do sexo " + pokemons[0][3] + " que tem o seu HP Maximo de " + pokemons[0][4]+ " esta com seu HP em " + pokemons[0][5] + "está cadastrado com sucesso")
 console.l1g(" O pokemon da posição " + pokemons[1][0] + " nomeado como " + pokemons[1][1] + " no level "+ pokemons[1][2] + " do sexo " + pokemons[1][3] + " que tem o seu HP Maximo de " + pokemons[1][4]+ " esta com seu HP em " + pokemons[1][5] + "está cadastrado com sucesso")
 console.l2g(" O pokemon da posição " + pokemons[2][0] + " nomeado como " + pokemons[2][1] + " no level "+ pokemons[2][2] + " do sexo " + pokemons[2][3] + " que tem o seu HP Maximo de " + pokemons[2][4]+ " esta com seu HP em " + pokemons[3][5] + "está cadastrado com sucesso")
@@ -15,3 +19,4 @@ console.l4g(" O pokemon da posição " + pokemons[4][0] + " nomeado como " + pok
 console.l5g(" O pokemon da posição " + pokemons[5][0] + " nomeado como " + pokemons[5][1] + " no level "+ pokemons[5][2] + " do sexo " + pokemons[5][3] + " que tem o seu HP Maximo de " + pokemons[5][4]+ " esta com seu HP em " + pokemons[5][5] + "está cadastrado com sucesso")
 console.l6g(" O pokemon da posição " + pokemons[6][0] + " nomeado como " + pokemons[6][1] + " no level "+ pokemons[6][2] + " do sexo " + pokemons[6][3] + " que tem o seu HP Maximo de " + pokemons[6][4]+ " esta com seu HP em " + pokemons[6][5] + "está cadastrado com sucesso")
 console.l7g(" O pokemon da posição " + pokemons[7][0] + " nomeado como " + pokemons[7][1] + " no level "+ pokemons[7][2] + " do sexo " + pokemons[7][3] + " que tem o seu HP Maximo de " + pokemons[7][4]+ " esta com seu HP em " + pokemons[7][5] + "está cadastrado com sucesso")
+
